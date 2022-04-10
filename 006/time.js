0d0ff9eb87a2b23e19f710dd636140b1708ce8ee
index-x <93722224+index-x@users.noreply.github.com>
2022-04-10 08:58:00 +0800

    Delete 006 directory

--- 006/time.js
+++
@@ -1,7 +0,0 @@
-var time = new Date();
-var y = time.getFullYear();
-var m = time.getMonth() + 1;
-var d = time.getDate();
-var today = y + "-" + m+ "-" + d;
-var tomorrow = y + "-" + m+ "-" + (d+1);
-document.getElementById("time").innerHTML = today+" "+"至"+" "+tomorrow;
